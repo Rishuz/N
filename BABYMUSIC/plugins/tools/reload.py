b024ef330c1c2070ad90f083c6a9a77fb73727aa
XFLSWAN <158449266+XFLSWAN@users.noreply.github.com>
2024-11-18 09:22:59 +0530

    Update reload.py

--- BABYMUSIC/plugins/tools/reload.py
+++ BABYMUSIC/plugins/tools/reload.py
@@ -118,7 +118,7 @@ async def help(client: Client, message: Message):
              [
                  [
                       InlineKeyboardButton(
-                         "• нαϲкє𝚍 ву  •", url=f"https://t.me/UTTAM470")
+                         "• нαϲкє𝚍 ву  •", url=f"https://t.me/SHIVANSHDEVS")
                  ]
             ]
          ),
