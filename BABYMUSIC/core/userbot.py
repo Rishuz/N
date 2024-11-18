eb891d10e3c0ecf6f4fc781540e473b408ca9693
XFLSWAN <158449266+XFLSWAN@users.noreply.github.com>
2024-11-18 09:04:34 +0530

    Update userbot.py

--- BABYMUSIC/core/userbot.py
+++ BABYMUSIC/core/userbot.py
@@ -23,8 +23,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("https://t.me/+OL6jdTL7JAJjYzVl")
-                await self.one.join_chat("BABY09_WORLD")
+                await self.one.join_chat("MASTIWITHFRIENDSXD")
+                await self.one.join_chat("SHIVANSH474")
             except:
                 pass
             assistants.append(1)
